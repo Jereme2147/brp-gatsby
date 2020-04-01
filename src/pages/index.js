b500a9b899e75c481d9075a0ed08a49affebda5e
jereme <jereme.daniels@gmail.com>
2020-04-01 11:06:42 -0400

    blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,9 @@ class IndexPage extends React.Component {
                             SERVING THE HIGH COUNTRY'S PROPANE NEEDS IN WESTERN NC AND EASTERN TN SINCE 1992
                         </h1>
                         {/* remove the following line after covid */}
-                        <h2 style={{color: `${variables.red}`, fontSize: '2rem', textAlign: 'center', marginTop: '20px', textShadow: `2px 2px 2px ${variables.white}`, padding: '5px'}}>We are practicing social Distancing!</h2>
+                        <h2 style={{color: `${variables.red}`, fontSize: '2rem', textAlign: 'center', marginTop: '20px', textShadow: `2px 2px 2px ${variables.white}`, padding: '5px'}}>
+                            We Are Practicing Social Distancing!
+                            </h2>
                         {/* remove above line */}
                         <div style={imgDivStyle}>
                             <img
